6c884f5f3da3c634fe917a81ca8b5cb0e447fa42
vexuroooxy <valdiazgahari91@gmail.com>
2025-12-03 10:21:24 +0700

    Add initial HTML structure and CSS styling for profile page

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profile</title>
+    
+    <!-- Style File -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Google+Sans+Flex:opsz,wght@6..144,1..1000&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <h1>My Profile</h1>
+    </header>
+    <div class="box">
+        <?php 
+        // declare variables
+        $name = "Valdiaz Gahari";
+        $nim = "251110206";
+        $class = "D2025";
+        $favnumber = 19;
+        $dreamGPA = 4.00;
+
+        // display data
+        echo "Name: $name <br>";
+        echo "NIM: $nim <br>";
+        echo "Class: $class <br>";
+        echo "Favorite Number: $favnumber <br>";
+        echo "Dream GPA: $dreamGPA <br>";
+    ?>
+    </div>
+    <footer>
+        <p>&copy; 2025 Valdiaz Gahari</p>
+    </footer>
+</body>
+</html>
